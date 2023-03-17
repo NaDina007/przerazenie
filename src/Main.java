43a513e65d7f96872ffaa62141d19ac3c623c02a
NaDina007 <nadya2017b@gmail.com>
2023-03-18 00:30:03 +0200

    10.11.2007. 16.09.2016. 24.02.2022. 10.11.2022. 10.02.2023. 13:19 - 13:26 15.03.2023.  ;)

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+        byte a = 1, b = 3;
+
+
+        double i = 2.5;
+
+        if ( a > i ) {
+            System.out.println("Nie wiem co mowic");
+        }
+        else if (b > i);{
+            System.out.println("Ohohoo");
+            }
+        }
+
+    }
